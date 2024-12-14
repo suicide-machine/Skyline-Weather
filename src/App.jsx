5d9b7d5f98834bba2ec4da22fdef71ad232194d4
Cenote <himangshukalita131@gmail.com>
2024-12-14 14:32:53 +0530

    install shadcnUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import { Button } from "@/components/ui/button"
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </>
   )
 }
 
